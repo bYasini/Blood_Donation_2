99188b07be3719d00524c4c41d4cff43c694f62e
bYasini <behzad.yasini.1999@gmail.com>
2023-01-17 11:37:32 +0430

    space added

--- database/migrations/2023_01_15_060955_create_blood_donation_table.php
+++ database/migrations/2023_01_15_060955_create_blood_donation_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('blood_donation', function (Blueprint $table) {
+        Schema::create('blood_donation', function (Blueprint $table) {  
             $table->id();
             $table->timestamps();
         });
